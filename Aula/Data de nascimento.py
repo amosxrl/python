2c0bf7944a789ec53b515031e3b5441fbc86ba9f
Amós Matias <amosxrl@users.github.com>
2020-05-06 19:51:46 -0300

    adição de arquivos
    o arquivo com as primeiras aulas do curso em video

---
+++ Aula/Data de nascimento.py
@@ -0,0 +1,5 @@
+print ('Escreva sua data de nascimento.')
+dia = input ('Dia: ')
+mes = input ('Mes: ')
+ano = input ('Ano: ')
+print ('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano, '. serto?')

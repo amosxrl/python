23e4bfa6832f203290143c89588094d1c30f135c
Amós Matias <amosxrl@users.github.com>
2020-05-29 15:24:15 -0300

    aula 6 com exercicios
    ex3 e ex4

---
+++ Aula/aula06.py
@@ -0,0 +1,10 @@
+inteiro = input('Valor a digitar: ')
+
+#"int"
+#"float"
+#"bool"
+#"str"
+#type()
+
+print(inteiro.isalnum())
+print(inteiro.isalpha())
---
+++ Aula/ex004.py
@@ -0,0 +1,4 @@
+a = input('digite: ')
+print('O tipo primitivo: ', type(a))
+print('Só tem espaço', a.isspace())
+print('Número?', a.isnumeric())

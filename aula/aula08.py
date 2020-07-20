335d2ed1850819152c3a75f019c5ef31e1642643
Amós Matias <amosxrl@github.com>
2020-07-20 13:31:45 -0300

    primeira modificação da aula 8 do curso em video.

---
+++ aula/aula08.py
@@ -0,0 +1,8 @@
+# math e uma biblioteca de recursos para calculo
+import emoji
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {:.2f} '.format(num, raiz))
+print(emoji.emojize("Olá, Mundo :sunglasses: :earth_americas: ", use_aliases=True))
+# ceil floor trunc pow sqrt factorial
